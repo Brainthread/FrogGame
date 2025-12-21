3c4da6072c74e38d9ab6c1902a9ee75528d0587b
Kei Duke Bergman <keidukebergman@gmail.com>
2025-12-21 17:31:51 +0100

    Update tongue_attack_handler.gd

--- scenes/tongue_attack_handler.gd
+++ scenes/tongue_attack_handler.gd
@@ -1,14 +1,30 @@
 extends Node3D
 
 var usable:bool = false
+var tongue_state:TongueState = TongueState.WAITING
+var tongue_target:Node3D = null
 @export var max_length:float = 1
 @export var extension_speed:float = 1
 @export var fall_to_floor_speed:float = 0.5
+@export var tongue_hitbox:Hitbox
 
-enum toungue_state {
+
+enum TongueState {
 	WAITING,
 	EXTENDING,
-	ATTACHED_GROUND
+	FALLING,
+	ATTACHED
 }
 
 signal attached(target:Node3D, position:Vector3)
+
+func _process(delta: float) -> void:
+	match tongue_state:
+		TongueState.WAITING:
+			pass
+		TongueState.EXTENDING:
+			pass
+		TongueState.FALLING:
+			pass
+		TongueState.ATTACHED:
+			pass
