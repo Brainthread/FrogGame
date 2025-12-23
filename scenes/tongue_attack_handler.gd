bf38d37610262efbbf9cd3488468aecdb3452764
Kei Duke Bergman <keidukebergman@gmail.com>
2025-12-23 14:19:07 +0100

    Add tongue tracking gameobjects

--- scenes/tongue_attack_handler.gd
+++ scenes/tongue_attack_handler.gd
@@ -3,10 +3,12 @@ extends Node3D
 var usable:bool = false
 var tongue_state:TongueState = TongueState.WAITING
 var tongue_target:Node3D = null
+var tongue_target_position:Vector3
 @export var max_length:float = 1
-@export var extension_speed:float = 1
+@export var extension_speed:float = 10
+@export var retraction_speed:float = 7
 @export var fall_to_floor_speed:float = 0.5
-@export var tongue_hitbox:Hitbox
+@export var tongue_tip_node:Node3D
 
 
 enum TongueState {
@@ -26,20 +28,32 @@ func _process(delta: float) -> void:
 		TongueState.WAITING:
 			if Input.is_action_just_pressed("tongue_attack"):
 				tongue_state = TongueState.EXTENDING
+				var cam = get_viewport().get_camera_3d()
+				var ray_origin = InputReader.get_mouse_world_origin(cam)
+				var ray_normal = InputReader.get_mouse_world_normal(cam)
+				var space_state = get_world_3d()
+				var ray_end = ray_origin + ray_normal * 100
+				var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
+				query.collide_with_areas = true
+				query.collide_with_bodies = true
+				var result = space_state.intersect_ray(query)
+				
+				
 		TongueState.EXTENDING:
-			if Input.is_action_just_released("tongue_attack"):
-				tongue_state = TongueState.RETRACTING
+			_test_for_retracting()
 		TongueState.RETRACTING:
-			if tongue_hitbox.position.distance_to(self.position) < 0.01:
+			if tongue_tip_node.position.distance_to(self.position) < 0.01:
 				tongue_state = TongueState.WAITING
 			else:
-				pass
+				tongue_tip_node.position = tongue_tip_node.position.move_toward(self.position, delta*retraction_speed) 
 		TongueState.FALLING:
-			if Input.is_action_just_released("tongue_attack"):
-				tongue_state = TongueState.RETRACTING
+			_test_for_retracting()
 		TongueState.ATTACHED:
-			if Input.is_action_just_released("tongue_attack"):
-				tongue_state = TongueState.RETRACTING
+			_test_for_retracting()
+
+func _test_for_retracting():
+	if Input.is_action_just_released("tongue_attack"):
+		tongue_state = TongueState.RETRACTING
 
 func _start_retracting():
 	tongue_state = TongueState.RETRACTING
--- scripts/InputReader.gd
+++ scripts/InputReader.gd
@@ -21,6 +21,16 @@ func _get_object_screen_position(object) -> Vector2:
 		var screen_pos = cam.unproject_position(object.global_transform.origin)
 		return screen_pos
 	return Vector2(NAN, NAN)
+	
+func get_mouse_world_origin(cam:Camera3D) -> Vector3:
+	var mouse_position = get_viewport().get_mouse_position()
+	var project_ray:Vector3 = cam.project_ray_origin(mouse_position)
+	return project_ray
+	
+func get_mouse_world_normal(cam:Camera3D) -> Vector3:
+	var mouse_position = get_viewport().get_mouse_position()
+	var project_ray:Vector3 = cam.project_ray_normal(mouse_position)
+	return project_ray
 
 func _get_mouse_object_offset(object) -> Vector2:
 	var screen_pos = _get_object_screen_position(object)
