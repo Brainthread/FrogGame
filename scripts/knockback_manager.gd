07396909e913a4433c6d24227122f8761b3e6e07
Kei <54135223+Brainthread@users.noreply.github.com>
2026-02-06 23:09:42 +0100

    Add basic knockback control

--- scripts/knockback_manager.gd
+++ scripts/knockback_manager.gd
@@ -7,5 +7,11 @@ func _ready() -> void:
 	get_parent().took_knockback.connect(on_take_knockback)
 
 func on_take_knockback (force:Vector3) -> void:
-	body.add_force(force)
-	print("knockback applied")
+	var current_velocity:Vector3 = body.velocity;
+	var mass = body.mass;
+	var target_velocity = force/mass
+	var diff = target_velocity - current_velocity
+	var force_to_add;
+	force_to_add = clamp(diff*mass, -force, force)
+	body.add_force(force_to_add)
+	print("knockback applied: ", force_to_add, ":", target_velocity, ":", current_velocity)
